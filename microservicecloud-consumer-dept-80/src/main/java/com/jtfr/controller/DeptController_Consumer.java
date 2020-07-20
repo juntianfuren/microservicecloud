3a973aab75e11b4cde2136f82eb15603b22c98bd
chenkangming <chenkangming@xsyxsc.com>
2020-07-20 08:53:01 +0800

    提交代码

--- microservicecloud-consumer-dept-80/src/main/java/com/jtfr/controller/DeptController_Consumer.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/jtfr/controller/DeptController_Consumer.java
@@ -3,7 +3,9 @@ package com.jtfr.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -23,20 +25,19 @@ public class DeptController_Consumer {
 	private RestTemplate restTemplate;
 
 	@ApiOperation("添加部门")
-	@RequestMapping(value = "/dept/add")
+	@PostMapping(value = "/dept/add")
 	public boolean add(Dept dept) {
 		return restTemplate.postForObject(REST_URL_PREFIX + "/dept/add", dept, Boolean.class);
 	}
 
 	@ApiOperation("获取指定部门")
-	@RequestMapping(value = "/dept/get/{id}")
+	@GetMapping(value = "/dept/get/{id}")
 	public Dept get(@PathVariable("id") Long id) {
 		return restTemplate.getForObject(REST_URL_PREFIX + "/dept/get/" + id, Dept.class);
 	}
 
 	@ApiOperation("查询所有部门")
-	@SuppressWarnings("unchecked")
-	@RequestMapping(value = "/dept/list")
+	@GetMapping(value = "/dept/list")
 	public List<Dept> list() {
 		return restTemplate.getForObject(REST_URL_PREFIX + "/dept/list", List.class);
 	}
